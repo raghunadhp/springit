442b4f7705cb8f85847137228aabd128a71a9562
Raghu <praghu91@gmail.com>
2019-02-03 23:38:31 +0530

    created entites and refactored with Lambok.

---
+++ src/main/java/com/raghu/springit/domain/Link.java
@@ -0,0 +1,50 @@
+package com.raghu.springit.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Link {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String title;
+	private String url;
+	
+	/*
+	 * public Link() {}
+	 * 
+	 * public Long getId() { return id; }
+	 * 
+	 * public void setId(Long id) { this.id = id; }
+	 * 
+	 * public String getTitle() { return title; }
+	 * 
+	 * public void setTitle(String title) { this.title = title; }
+	 * 
+	 * public String getUrl() { return url; }
+	 * 
+	 * public void setUrl(String url) { this.url = url; }
+	 * 
+	 * @Override public String toString() { return "Link {" + "id="+ id +
+	 * ", title='" + title + '\''+ '}'; }
+	 * 
+	 * 
+	 * @Override public boolean equals(Object obj) { if(this == obj) return true;
+	 * if(obj == null || getClass() != obj.getClass()) return false; Link link =
+	 * (Link) obj; return Objects.equals(id, link.id) && Objects.equals(title,
+	 * link.title); }
+	 * 
+	 * @Override public int hashCode() { return Objects.hash(id, title); }
+	 */
+
+}
